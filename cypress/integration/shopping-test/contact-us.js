6667ae77a4db63189c90bfac06d92ffc676a82fa
Punam-Thapa <punam.thapa@introcept.co>
2022-03-11 17:38:29 +0545

    selectors

--- cypress/integration/shopping-test/contact-us.js
+++ cypress/integration/shopping-test/contact-us.js
@@ -2,13 +2,36 @@
 describe("Test Contact Us for shopping website", () => {
     it("Should be able to navigate to contact page", () => {
         cy.visit('https://automationteststore.com/');
-        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        
+        //withnoxpath
+
+        // cy.get('.info_links_footer > :nth-child(5) > a').click();
+        //input[contains(@id,'ContactUsFrm_first_name')]
+        // cy.get('#ContactUsFrm_first_name').type("Anushka");
+        // cy.get('#ContactUsFrm_email').type("Sharma@gmail.com");
+        // cy.get('#ContactUsFrm_enquiry').type("everything good...");
+        // cy.get('.col-md-6 > .btn').click();
+
+        //usingxpath
+
+        // cy.xpath(' //a[contains(@href,"contact")]').click();
+
+        // cy.xpath('//input[contains(@id,"ContactUsFrm_first_name")]').type('anushka');
+
+        // cy.xpath('//input[contains(@id,"ContactUsFrm_email")]').type('anuskha@gmail.com');
+
+        // cy.xpath('//textarea[contains(@id,"ContactUsFrm_enquiry")]').type("hellllllloooooo");
+
+        // cy.xpath('//button[contains(@class,"btn btn-primary lock-on-click")]').click();
+
+        //using dyanamic paths
+        cy.get("a[href$='contact']").click();
         cy.get('#ContactUsFrm_first_name').type("Anushka");
         cy.get('#ContactUsFrm_email').type("Sharma@gmail.com");
         cy.get('#ContactUsFrm_enquiry').type("everything good...");
-        cy.get('.col-md-6 > .btn').click();
+        cy.get("button[title='Submit']").click();
 
-    
+
 
 
 
@@ -19,13 +42,16 @@ describe("Test Contact Us for shopping website", () => {
 
     it("for invalid email", () => {
         cy.visit('https://automationteststore.com/');
-        cy.get('.info_links_footer > :nth-child(5) > a').click();
-        cy.get('#ContactUsFrm_first_name').type("Anushka");
-        cy.get('#ContactUsFrm_email').type("a");
-        cy.get('#ContactUsFrm_enquiry').type("everything good...");
-        cy.get('.col-md-6 > .btn').click();
+        //noxpath
+        //     cy.get('.info_links_footer > :nth-child(5) > a').click();
+        //     cy.get('#ContactUsFrm_first_name').type("Anushka");
+        //     cy.get('#ContactUsFrm_email').type("a");
+        //     cy.get('#ContactUsFrm_enquiry').type("everything good...");
+        //     cy.get('.col-md-6 > .btn').click();
 
-    
+
+
+
 
 
 
@@ -33,15 +59,25 @@ describe("Test Contact Us for shopping website", () => {
 
 
     });
-    it.only("to reset the contact details", () => {
+    it("to reset the contact details", () => {
         cy.visit('https://automationteststore.com/');
-        cy.get('.info_links_footer > :nth-child(5) > a').click();
-        cy.get('#ContactUsFrm_first_name').type("Anushka");
-        cy.get('#ContactUsFrm_email').type("a");
-        cy.get('#ContactUsFrm_enquiry').type("everything good...");
-        cy.get('.col-md-1 > .btn').click();
+        //no xpath
+        //     cy.get('.info_links_footer > :nth-child(5) > a').click();
+        //     cy.get('#ContactUsFrm_first_name').type("Anushka");
+        //     cy.get('#ContactUsFrm_email').type("a");
+        //     cy.get('#ContactUsFrm_enquiry').type("everything good...");
+        //     cy.get('.col-md-1 > .btn').click();
 
-    
+        //using xpath
+        cy.xpath(' //a[contains(@href,"contact")]').click();
+
+        cy.xpath('//input[contains(@id,"ContactUsFrm_first_name")]').type('anushka');
+
+        cy.xpath('//input[contains(@id,"ContactUsFrm_email")]').type('anuskha@gmail.com');
+
+        cy.xpath('//textarea[contains(@id,"ContactUsFrm_enquiry")]').type("hellllllloooooo");
+        cy.xpath('//button[contains(@class,"btn btn-default pull-left")]').click();
+
 
 
 
