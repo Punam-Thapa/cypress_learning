e3071639e1c0aa816f370368514042221fd60d90
Punam-Thapa <punam.thapa@introcept.co>
2022-03-25 12:25:39 +0545

    check and validate radiobuttons

---
+++ cypress/integration/webdriver_uni/radiobuttons.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+describe(" verify radiobuttons", () => {
+    it("check and validate radiobuttons", () => {
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click();
+        cy.get('#radio-buttons').find("[type='radio']").first().check();
+        cy.get('#radio-buttons').find("[type='radio']").eq(1).check();
+    })
+    it.only("check and validate radiobuttons", () => {
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click();
+        // cy.get('#radio-buttons-selected-disabled').find("[type='radio']").first().should('not.be.checked')
+        cy.get('[value="lettuce"]').should('not.be.checked');
+        cy.get('[value="cabbage"]').should('be.disabled');
+        cy.get('[value="pumpkin"]').should('be.checked');
+
+        })
+
+
+})
