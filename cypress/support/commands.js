7bb5a4795bc361fcb21ba23357c869277fea9e7f
Punam-Thapa <punam.thapa@introcept.co>
2022-04-08 14:08:27 +0545

    commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,6 +27,15 @@ Cypress.Commands.add("selectProduct", productName => {
     });
 })
 
+Cypress.Commands.add("webdriveruni", (firtsName,lastName,email,comment,$selector,textToLocate) => {
+    cy.get('[name="first_name"]').type(firtsName);
+        cy.get('[name="last_name"]').type(lastName);
+        cy.get('[name="email"]').type(email)
+        cy.get('textarea.feedback-input').type(comment)
+        cy.get('[type="submit"]').click();
+        cy.get($selector).contains(textToLocate)
+    });
+
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 import 'cypress-file-upload';
